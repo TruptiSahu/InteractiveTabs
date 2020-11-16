a124ce2e180a38d8a945bf898b134463d68e5394
TruptiSahu <truptiranjansahu2@gmail.com>
2020-11-16 10:03:50 -0800

    All done

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ const tabs = document.getElementsByClassName('tab');
 const contents = document.getElementsByClassName('content');
 
 for (let i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener('click', () => {});
+  tabs[i].addEventListener('click', () => {
+    for (let j = 0; j < contents.length; j++) {
+      contents[j].className = 'content';
+
+      contents[i].className = 'content active';
+    }
+  });
 }
